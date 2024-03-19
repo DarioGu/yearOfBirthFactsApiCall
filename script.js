8ea1b3c3dc7861fa1c9e0325098252e322350154
Dario G <134203714+DarioGu@users.noreply.github.com>
2024-03-19 23:29:48 +0100

    initial upload of project

---
+++ script.js
@@ -0,0 +1,11 @@
+// get the text from the text input field when the submit button is clicked or the enter key is hit and save it in a variable
+
+const userYear = document.getElementById('userYear').value;
+
+
+//transfer the text value into the API Url
+
+//get the generated text for the label from the api and save it in a variable
+
+//display the generated text from the api in the label on the webpage
+
